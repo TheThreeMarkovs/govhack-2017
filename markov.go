bdb876a008b5a7745dd002cf71fbbe69707d7d60
Christian Kiely <ckiely91@gmail.com>
2017-07-29 15:10:39 +1000

    Use counts/probabilities rather than huge rune arrays

--- markov.go
+++ markov.go
@@ -5,14 +5,45 @@ import (
 	"strings"
 )
 
+type runeCounts struct {
+	countMap   map[rune]int
+	totalCount int
+}
+
+func (counts *runeCounts) addRune(c rune) {
+	if _, ok := counts.countMap[c]; ok {
+		counts.countMap[c]++
+	} else {
+		counts.countMap[c] = 1
+	}
+	counts.totalCount++
+}
+
+func (counts *runeCounts) getRandomRune() rune {
+	// Use count weightings to determine
+	var probSum float64 = 0
+	randProb := rand.Float64()
+
+	var chosenChar rune
+	for chosenChar, count := range counts.countMap {
+		thisProbRange := float64(count) / float64(counts.totalCount)
+		if probSum < randProb && randProb < probSum+thisProbRange {
+			return chosenChar
+		}
+		probSum += thisProbRange
+	}
+
+	return chosenChar
+}
+
 type Markov struct {
-	chain map[string][]rune
+	chain map[string]runeCounts
 	n     int
 }
 
 func NewMarkov(n int) *Markov {
 	return &Markov{
-		chain: map[string][]rune{},
+		chain: map[string]runeCounts{},
 		n:     n,
 	}
 }
@@ -27,10 +58,14 @@ func (m *Markov) ParseWord(word string) {
 	for i := 0; i < end; i++ {
 		key := string(runes[i : i+m.n])
 		val := runes[i+m.n]
-		if _, ok := m.chain[key]; ok {
-			m.chain[key] = append(m.chain[key], val)
+		runeCount, ok := m.chain[key]
+		if ok {
+			runeCount.addRune(val)
 		} else {
-			m.chain[key] = []rune{val}
+			m.chain[key] = runeCounts{
+				countMap:   map[rune]int{val: 1},
+				totalCount: 1,
+			}
 		}
 	}
 }
@@ -42,7 +77,7 @@ func (m *Markov) GenerateWord(length int) string {
 		lastLetters := letters[i-m.n : i]
 		// fmt.Printf("Looking for key %v\n", string(lastLetters))
 		if runes, ok := m.chain[string(lastLetters)]; ok {
-			letters = append(letters, getRandomLetter(runes))
+			letters = append(letters, runes.getRandomRune())
 		} else {
 			return string(letters)
 		}
